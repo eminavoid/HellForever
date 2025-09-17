beb40729a8e1d1e7247db394310b85084084b13a
Carlos Emilio Guidi Cordero <emiliogc06@gmail.com>
2025-09-17 17:12:52 -0300

    alpha push

--- Assets/FPS/Scripts/Game/Managers/GameFlowManager.cs
+++ Assets/FPS/Scripts/Game/Managers/GameFlowManager.cs
@@ -5,13 +5,15 @@ namespace Unity.FPS.Game
 {
     public class GameFlowManager : MonoBehaviour
     {
-        [Header("Parameters")] [Tooltip("Duration of the fade-to-black at the end of the game")]
+        [Header("Parameters")]
+        [Tooltip("Duration of the fade-to-black at the end of the game")]
         public float EndSceneLoadDelay = 3f;
 
         [Tooltip("The canvas group of the fade-to-black screen")]
         public CanvasGroup EndGameFadeCanvasGroup;
 
-        [Header("Win")] [Tooltip("This string has to be the name of the scene you want to load when winning")]
+        [Header("Win")]
+        [Tooltip("This string has to be the name of the scene you want to load when winning")]
         public string WinSceneName = "WinScene";
 
         [Tooltip("Duration of delay before the fade-to-black, if winning")]
@@ -24,14 +26,18 @@ namespace Unity.FPS.Game
 
         [Tooltip("Sound played on win")] public AudioClip VictorySound;
 
-        [Header("Lose")] [Tooltip("This string has to be the name of the scene you want to load when losing")]
+        [Header("Lose")]
+        [Tooltip("This string has to be the name of the scene you want to load when losing")]
         public string LoseSceneName = "LoseScene";
-
 
         public bool GameIsEnding { get; private set; }
 
         float m_TimeLoadEndGameScene;
-        string m_SceneToLoad;
+
+        // New: allow loading either by name or by build index
+        string m_SceneToLoadByName;
+        int m_SceneToLoadByIndex = -1;
+
         void Awake()
         {
             EventManager.AddListener<AllObjectivesCompletedEvent>(OnAllObjectivesCompleted);
@@ -55,7 +61,11 @@ namespace Unity.FPS.Game
                 // See if it's time to load the end scene (after the delay)
                 if (Time.time >= m_TimeLoadEndGameScene)
                 {
-                    SceneManager.LoadScene(m_SceneToLoad);
+                    if (m_SceneToLoadByIndex >= 0)
+                        SceneManager.LoadScene(m_SceneToLoadByIndex);
+                    else
+                        SceneManager.LoadScene(m_SceneToLoadByName);
+
                     GameIsEnding = false;
                 }
             }
@@ -73,9 +83,22 @@ namespace Unity.FPS.Game
             // Remember that we need to load the appropriate end scene after a delay
             GameIsEnding = true;
             EndGameFadeCanvasGroup.gameObject.SetActive(true);
+
             if (win)
             {
-                m_SceneToLoad = WinSceneName;
+                // --- NEW: next-level chain 3->4->5->0, else fallback to WinSceneName ---
+                if (TryGetNextLevelIndex(out int nextIndex))
+                {
+                    m_SceneToLoadByIndex = nextIndex;
+                    m_SceneToLoadByName = null;
+                }
+                else
+                {
+                    m_SceneToLoadByIndex = -1;
+                    m_SceneToLoadByName = WinSceneName;
+                }
+                // ------------------------------------------------------------------------
+
                 m_TimeLoadEndGameScene = Time.time + EndSceneLoadDelay + DelayBeforeFadeToBlack;
 
                 // play a sound on win
@@ -85,14 +108,7 @@ namespace Unity.FPS.Game
                 audioSource.outputAudioMixerGroup = AudioUtility.GetAudioGroup(AudioUtility.AudioGroups.HUDVictory);
                 audioSource.PlayScheduled(AudioSettings.dspTime + DelayBeforeWinMessage);
 
-                // create a game message
-                //var message = Instantiate(WinGameMessagePrefab).GetComponent<DisplayMessage>();
-                //if (message)
-                //{
-                //    message.delayBeforeShowing = delayBeforeWinMessage;
-                //    message.GetComponent<Transform>().SetAsLastSibling();
-                //}
-
+                // show win message
                 DisplayMessageEvent displayMessage = Events.DisplayMessageEvent;
                 displayMessage.Message = WinGameMessage;
                 displayMessage.DelayBeforeDisplay = DelayBeforeWinMessage;
@@ -100,8 +116,24 @@ namespace Unity.FPS.Game
             }
             else
             {
-                m_SceneToLoad = LoseSceneName;
+                m_SceneToLoadByIndex = -1;
+                m_SceneToLoadByName = LoseSceneName;
                 m_TimeLoadEndGameScene = Time.time + EndSceneLoadDelay;
+            }
+        }
+
+        // NEW: Exact next-level mapping as requested
+        bool TryGetNextLevelIndex(out int nextIndex)
+        {
+            int current = SceneManager.GetActiveScene().buildIndex;
+            switch (current)
+            {
+                case 3: nextIndex = 4; return true;
+                case 4: nextIndex = 5; return true;
+                case 5: nextIndex = 0; return true;
+                default:
+                    nextIndex = -1;
+                    return false; // fallback to WinSceneName
             }
         }
 
