83dbe47455bdf11e932729e9a02b7f804c1f6f78
Alan Codini <alancodini@gmail.com>
2025-09-16 22:30:16 -0300

    added assembly ref

---
+++ Assets/FPS/Scripts/ScriptsPropios/TDA/QueueTDA.cs
@@ -0,0 +1,39 @@
+namespace Unity.FPS.ours
+{
+    public class QueueTDA<TEntity> : IQueueTDA<TEntity>
+    {
+        TEntity[] a; // arreglo en donde se guarda la informacion
+        int indice; // variable entera en donde se guarda la cantidad de elementos que se tienen guardados
+
+        public void InicializarCola(int cantidad)
+        {
+            a = new TEntity[cantidad];
+            indice = 0;
+        }
+
+        public void Acolar(TEntity x)
+        {
+            for (int i = indice - 1; i >= 0; i--)
+            {
+                a[i + 1] = a[i];
+            }
+            a[0] = x;
+
+            indice++;
+        }
+        public void Desacolar()
+        {
+            indice--;
+        }
+
+        public bool ColaVacia()
+        {
+            return (indice == 0);
+        }
+
+        public TEntity Primero()
+        {
+            return a[indice - 1];
+        }
+    }
+}
---
+++ Assets/FPS/Scripts/ScriptsPropios/TDA/StackTDA.cs
@@ -0,0 +1,65 @@
+using System;
+using UnityEngine;
+
+namespace Unity.FPS.ours
+{
+    public class StackTDA<TEntity> : IStackTDA<TEntity>
+    {
+        // arreglo en donde se guarda la informacion
+        TEntity[] a;
+        // cantidad de anillos de datos de la pila
+        int cantidad_datos_max;
+        // variable entera en donde se guarda la cantidad de elementos que se tienen guardados
+        int indice;
+
+        public void InicializarPila(int cantidad)
+        {
+            cantidad_datos_max = cantidad;
+            a = new TEntity[cantidad];
+            indice = 0;
+        }
+
+        public int Apilar(TEntity x)
+        {
+            if (indice < cantidad_datos_max)
+            {
+                a[indice] = x;
+                indice++;
+                return indice;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        public void Desapilar()
+        {
+            if (!PilaVacia())
+            {
+                indice--;
+            }
+        }
+
+        public bool PilaVacia()
+        {
+            return (indice == 0);
+        }
+
+        public TEntity Tope()
+        {
+            return a[indice - 1];
+        }
+
+        public void imprimoPila()
+        {
+            for (int i = indice - 1; i >= 0; i--)
+            {
+                Console.WriteLine("Elemento: " + a[i]);
+            }
+
+        }
+    }
+}
+
